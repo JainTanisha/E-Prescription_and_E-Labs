7eff73eac98b2448847fc1ac7352ba20998c88a5
Tanisha_Jain <jain.tan@husky.neu.edu>
2017-07-06 19:01:50 -0400

    Java Desktop Application

---
+++ Final Project/src/Business/Role/SystemAdminRole.java
@@ -0,0 +1,27 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Business.Role;
+
+import Business.EcoSystem;
+import Business.Enterprise.Enterprise;
+import Business.Network.Network;
+import Business.Organization.Organization;
+import Business.UserAccount.UserAccount;
+import UserInterface_SystemAdministrativeRole.SystemAdminWorkAreaJPanel;
+import javax.swing.JPanel;
+
+/**
+ *
+ * @author Tanisha_Jain
+ */
+public class SystemAdminRole extends Role{
+
+    @Override
+    public JPanel createWorkArea(JPanel userProcessContainer, UserAccount account,
+            Organization organization, Enterprise enterprise, EcoSystem system, Network network) {
+        return new SystemAdminWorkAreaJPanel(userProcessContainer, system);
+    }
+    
+}
